a54fb1ec78c13d53ed966a9c9e307dd41b7dcf45
JXiong27 <jxiong3@atu.edu>
2024-08-29 10:12:56 -0500

    Update gitPractice.cpp

--- gitPractice/gitPractice.cpp
+++ gitPractice/gitPractice.cpp
@@ -1,5 +1,5 @@
 // gitPractice.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Jordan Xiong
 
 #include <iostream>
 
@@ -8,5 +8,6 @@ using namespace std;
 int main()
 {
     cout << "Hello World!" << endl;
+    cout << "How are you" << endl;
 }
 
